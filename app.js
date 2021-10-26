4f060ef37da501f945b92632dc1bc1baca1c5f00
kwaku fosu <kwakuboachiefosu@gmail.com>
2021-10-26 19:54:38 +0000

    initial commit

---
+++ app.js
@@ -0,0 +1,57 @@
+const express= require('express')
+const app=express();
+const {graphqlHTTP}= require('express-graphql')
+const {GraphQLObjectType, GraphQLInt, GraphQLString,GraphQLList, GraphQLSchema}=require('graphql')
+
+const blogData=[
+    {id:1, content:"Blog Post 1",author:'Sam'},
+    {id:2, content:"Blog Post 2",author:'Mou'},
+    {id:3, content:"Blog Post 3",author:'Gad'}
+]
+
+//Schema
+//resolver
+
+const blogType= new GraphQLObjectType({
+    name:'Blog',
+    description:'Fetch your favorite blogpost',
+    fields:{
+        id:{
+            type:GraphQLInt
+        },
+
+        content:{
+            type:GraphQLString
+        },
+        
+        author:{
+            type:GraphQLString
+        }
+    }
+
+})
+
+const rootQuery = new GraphQLObjectType({
+    name:'RootQuery',
+    description:'This is the rootquery',
+    fields:{
+        blog:{
+            type:GraphQLList(blogType),
+            resolve:()=> blogData   
+        }
+    }
+
+})
+
+const schema= new GraphQLSchema({query:rootQuery})
+
+app.use('/graphql',graphqlHTTP({
+     schema,
+     graphiql:true 
+}))
+
+const PORT= process.env.port || 3000
+
+app.listen(PORT,()=>{
+    console.log(`Listening on port ${PORT}`)
+})
