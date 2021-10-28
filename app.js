325f6921e8b25516105da19a25a2039c0193209f
kwaku fosu <kwakuboachiefosu@gmail.com>
2021-10-28 11:56:08 +0000

    Corrected a few errors and added required features

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
+const e = require('express');
 const express= require('express')
 const app=express();
 const {graphqlHTTP}= require('express-graphql')
 const {GraphQLObjectType, GraphQLInt, GraphQLString,GraphQLList, GraphQLSchema}=require('graphql')
 
 const blogData=[
-    {id:0,banner:"B1", content:"Blog Post 1",author:'Sam',about:'My name is sam. I enjoy blogging ', likes:2,unlikes:1},
-    {id:1,banner:"B2", content:"Blog Post 2",author:'Mou',about:'My name is Mou. I enjoy blogging ',likes:3,unlikes:2},
-    {id:2,banner:"B3", content:"Blog Post 3",author:'Gad',about:'My name is Gad. I enjoy blogging ',likes:2,unlikes:0}
+    {id:0,banner:"B1", content:"Blog Post 1",author:'Sam',about:'My name is sam. I enjoy blogging ', likes:2,unlikes:1,comment:"I love this",replies:"same here"},
+    {id:1,banner:"B2", content:"Blog Post 2",author:'Mou',about:'My name is Mou. I enjoy blogging ',likes:3,unlikes:2,comment:"I like this",replies:"You do?"},
+    {id:2,banner:"B3", content:"Blog Post 3",author:'Gad',about:'My name is Gad. I enjoy blogging ',likes:2,unlikes:0,comment:"You can do better",replies:"Thanks for the feedback"}
 ]
 
 //Schema
@@ -38,6 +39,12 @@ const blogType= new GraphQLObjectType({
         },
         unlikes:{
             type:GraphQLInt
+        },
+        comment:{
+            type: GraphQLString
+        },
+        replies:{
+            type: GraphQLString
         }
 
         
@@ -65,12 +72,12 @@ const rootQuery = new GraphQLObjectType({
 })
 
 //Mutations
-const rootMutation = new GraphQLObjectType({
-    name:'RootMutation',
-    description:'This is the rootmutation',
+
+const mutants = new GraphQLObjectType({
+    name:'Mutations',
+    description:'This is the Likemutation',
     fields:{
-       
-        blog:{
+        createBlog:{
             type:blogType,
             args:{
                 
@@ -93,11 +100,18 @@ const rootMutation = new GraphQLObjectType({
                 },
                 unlikes:{
                     type:GraphQLInt
+                },
+                comment:{
+                    type:GraphQLString
+                },
+                replies:{
+                    type:GraphQLString
                 }
                 
+                
                 },
-                resolve:(_,{banner,content,author,about,likes,unlikes})=> {
-                    const newBlogPost={id:blogData.length+1,banner:banner, content:content,author:author,about:about, likes:likes,unlikes:unlikes}
+                resolve:(_,{banner,content,author,about})=> {
+                    const newBlogPost={id:blogData.length,banner:banner, content:content,author:author,about:about,likes:likes=0,unlikes:unlikes=0,comment:comment="",replies:replies=""}
                     blogData.push(newBlogPost)
                     return newBlogPost
             }
@@ -105,18 +119,6 @@ const rootMutation = new GraphQLObjectType({
 
             
         },
-
-       
-
-       
-    }
-   
-
-})
-const likeMutation_and_unlike = new GraphQLObjectType({
-    name:'Likemutaion',
-    description:'This is the Likemutation',
-    fields:{
        
         likeBlog:{
             type:blogType,
@@ -128,11 +130,16 @@ const likeMutation_and_unlike = new GraphQLObjectType({
                 
               },
               resolve:(_,{id})=>{
-                const likeProfile= blogData[id]
-                console.log(likeProfile.likes)
-                likeProfile.likes=likeProfile.likes+1
-                console.log(likeProfile.likes)
-                return likeProfile
+                    
+                    const likeProfile= blogData[id]
+                   
+                    likeProfile.likes=likeProfile.likes+1
+                    
+                    return likeProfile
+                                            
+                   
+                
+                
             }
         },
 
@@ -170,6 +177,111 @@ const likeMutation_and_unlike = new GraphQLObjectType({
             
 
             
+        },
+
+        updateBlog:{
+            type:blogType,
+            args:{
+                
+                id:{
+                    type: GraphQLInt
+                },
+                banner:{
+                    type:GraphQLString
+                },
+        
+                content:{
+                    type:GraphQLString
+                }
+                
+                
+                },
+                resolve:(_,{id,banner,content})=> {
+                    const blogUpdateData=  blogData[id]
+                    blogUpdateData.content= content
+                    blogUpdateData.banner= banner
+                     return blogUpdateData
+            }
+            
+
+            
+        },
+
+        commentBlog:{
+            type:blogType,
+            args:{
+                
+                id:{
+                    type: GraphQLInt
+                },
+                comment:{
+                    type:GraphQLString
+                },
+        
+                
+                
+                
+                },
+                resolve:(_,{id,comment})=> {
+                    const blogCommentData=  blogData[id]
+                    blogCommentData.comment= blogCommentData.comment.concat(",",comment)
+                    
+                     return blogCommentData
+            }
+            
+
+            
+        },
+
+        deleteCommentBlog:{
+            type:blogType,
+            args:{
+                
+                id:{
+                    type: GraphQLInt
+                },
+                comment:{
+                    type:GraphQLString
+                },
+        
+                
+                
+                
+                },
+                resolve:(_,{id,comment})=> {
+                    const blogDelComment=  blogData[id]
+                    blogDelComment.comment= blogDelComment.comment.replace((","+comment)," ")
+                    
+                     return blogDelComment
+            }
+
+            
+            
+
+            
+        },
+        replyCommentBlog:{
+            type:blogType,
+            args:{
+                
+                id:{
+                    type: GraphQLInt
+                },
+                replies:{
+                    type:GraphQLString
+                },
+        
+                
+                
+                
+                },
+                resolve:(_,{id,replies})=> {
+                    const replyComment=  blogData[id]
+                    
+                    replyComment.replies= replyComment.replies.concat(",",replies)
+                    
+                     return replyComment
+            }
         }
         
     }
@@ -177,7 +289,7 @@ const likeMutation_and_unlike = new GraphQLObjectType({
 })
 
 
-const schema= new GraphQLSchema({query:rootQuery,mutation:rootMutation,mutation:likeMutation_and_unlike})
+const schema= new GraphQLSchema({query:rootQuery,mutation:mutants})
 
 app.use('/',graphqlHTTP({
      schema,
